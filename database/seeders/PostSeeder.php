0fbc5f3b843a29555eeb49c867a1911ab8d7fffa
Alessio Vietri <info@alessiovietri.it>
2024-10-28 10:34:29 +0000

    Completato seeding

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Models
+use App\Models\Post;
+
 class PostSeeder extends Seeder
 {
     /**
@@ -12,6 +15,19 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Post::truncate();
+
+        for ($i = 0; $i < 10; $i++) {
+            $name = fake()->sentence();
+            $slug = str_replace([' ', '.', ','], ['-', '', ''], strtolower($name));
+            Post::create([
+                'title' => $name,
+                'slug' => $slug,
+                'content' => fake()->paragraph(),
+                'cover' => fake()->optional()->imageUrl(),
+                'likes' => rand(0, 999),
+                'published' => fake()->boolean(70),
+            ]);
+        }
     }
 }
