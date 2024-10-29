45a30e4742577d2620bc40c2a7be4f17bf94a4fe
Alessio Vietri <info@alessiovietri.it>
2024-10-29 10:16:23 +0100

    Aggiunta migration aggiornamento posts

---
+++ database/migrations/2024_10_29_091010_add_category_id_to_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            /* Crea la colonna category_id ->  */
+            $table->unsignedBigInteger('category_id')->nullable();
+
+            /* Aggiunge la foreign key sulla colonna category_id */
+            $table->foreign('category_id')
+                    ->references('id')
+                    ->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            if (Schema::hasColumn('posts', 'category_id')) {
+                /* PRIMA rimuovo il vincolo di foreign key (altrimenti non posso droppare la colonna) */
+                // $table->dropForeign('posts_category_id_foreign');
+                // OPPURE
+                $table->dropForeign(['category_id']);
+
+                /* E poi droppo la colonna */
+                $table->dropColumn('category_id');
+            }
+        });
+    }
+};
